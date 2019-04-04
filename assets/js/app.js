d031c975dd6348da6741de74e64fb9fe1d9c8b3e
justbouton <justb1985@gmail.com>
2019-04-04 14:42:44 -0700

    Need to push more...

---
+++ assets/js/app.js
@@ -0,0 +1,23 @@
+// li toggle class for line-through
+$("ul").on("click", "li", function () {
+    $(this).toggleClass("completed");
+});
+
+// Span on click fadeOut parent cb remove()
+$("ul").on("click", "span", function(event) {
+    $(this).parent().fadeOut(100, function(){
+        $(this).remove();
+    });
+    // Stop from bubbling up to other elements
+    event.stopPropagation();
+});
+
+$("input[type='text']").keypress(function(event) {
+    if(event.which === 13){
+        // capture text from input store in var
+        var newToDo = $(this).val();
+        $(this).val("");        
+        // create new li and add to ul
+        $("ul").append("<li><span><i class='fas fa-trash-alt'></i></span> " + newToDo + "</li>");
+    }
+})
